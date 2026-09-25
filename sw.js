c13b682bfb44e722655d077bb924ff10bbc674f9
GMM <stellamoore1244@gmail.com>
2026-09-25 20:45:15 +0100

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,15 +1,35 @@
-const CACHE_NAME = "manny-mfb-v1";
+const CACHE_NAME = "manny-mfb-v2";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.svg",
+  "./icon-512.svg"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+      return cache.addAll(FILES);
     })
   );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
